e9f6783d1906bf9740cc0c481a62ef63d65a8fc2
wanly13 <wanlyobregon13@gmail.com>
2024-09-21 12:11:09 -0500

    mdulo evento

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const API_SERVICE_WEB ='https://backend.mispueblitos.info';
 export const API_SERVICE_SEGURIDAD ='https://backend.mispueblitos.info';
 export const API_SERVICE_COMUN ='https://backend.mispueblitos.info';
 //export const API_SERVICE_ADMIN ='http://localhost:3001/';
-export const API_SERVICE_ADMIN ='https://mis-pueblitos-backend.onrender.com/';
+export const API_SERVICE_ADMIN ='https://mis-pueblitos-backend-admin.onrender.com/';
 //'http://localhost:3001'
 
